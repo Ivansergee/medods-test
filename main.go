cbef5fe43c2bfae5f32f00ceb146e7ac50c126e8
ivan klimentev <klimentievis@inbox.ru>
2023-08-18 13:19:07 +0300

    implemented /token and /token/refresh endpoints

---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID               string
+	RefreshTokenHash string
+}
+
+type TokenReq struct {
+	ID string `json:"id"`
+}
+
+type TokenResp struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RefreshTokenReq struct {
+	ID           string `json:"id"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+var jwtSecretKey = []byte("secret-key")
+
+var users = map[string]User{}
+
+func GenerateTokens(sub string) (string, string, string, error) {
+	payload := jwt.MapClaims{
+		"sub": sub,
+		"exp": time.Now().Add(time.Minute * 15).Unix(),
+	}
+
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS512, payload)
+	accessTokenStr, err := accessToken.SignedString(jwtSecretKey)
+
+	if err != nil {
+		return "", "", "", errors.New("jwt error")
+	}
+
+	refreshToken := make([]byte, 32)
+	if _, err := rand.Read(refreshToken); err != nil {
+		return "", "", "", errors.New("rand error")
+	}
+	refreshTokenStr := base64.StdEncoding.EncodeToString(refreshToken)
+	refreshTokenHash, err := bcrypt.GenerateFromPassword([]byte(refreshTokenStr), bcrypt.DefaultCost)
+	if err != nil {
+		return "", "", "", errors.New("bcrypt error")
+	}
+
+	return accessTokenStr, refreshTokenStr, string(refreshTokenHash), nil
+}
+
+func main() {
+	users["foobar"] = User{ID: "foobar"}
+
+	app := fiber.New()
+	app.Post("/token", func(c *fiber.Ctx) error {
+		req := TokenReq{}
+		err := c.BodyParser(&req)
+		if err != nil {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"message": "invalid json"})
+		}
+
+		user, ok := users[req.ID]
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "invalid credentials"})
+		}
+
+		accessToken, refreshToken, refreshTokenHash, err := GenerateTokens(user.ID)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		user = User{
+			ID:               user.ID,
+			RefreshTokenHash: refreshTokenHash,
+		}
+		users[user.ID] = user
+		res := TokenResp{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+		}
+
+		return c.JSON(res)
+	})
+	app.Post("/token/refresh", func(c *fiber.Ctx) error {
+		req := RefreshTokenReq{}
+		err := c.BodyParser(&req)
+		if err != nil {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"message": "invalid json"})
+		}
+
+		user, ok := users[req.ID]
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "invalid credentials"})
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(user.RefreshTokenHash), []byte(req.RefreshToken))
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "invalid token"})
+		}
+
+		accessToken, refreshToken, refreshTokenHash, err := GenerateTokens(user.ID)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		user.RefreshTokenHash = refreshTokenHash
+		users[req.ID] = user
+
+		res := TokenResp{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+		}
+
+		return c.JSON(res)
+
+	})
+
+	app.Listen(":8000")
+}
